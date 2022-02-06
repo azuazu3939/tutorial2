dc35e9ee3ca003f06d15d551e5cedc5efd2bc683
azuazu3939 <98195581+azuazu3939@users.noreply.github.com>
2022-02-07 05:24:45 +0900

    Add files via upload

--- main/java/azu1/azu1/Azu2.java
+++ main/java/azu1/azu1/Azu2.java
@@ -1,15 +1,12 @@
 package azu1.azu1;
 
 import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collections;
-import java.util.List;
 
 
 public final class Azu2 extends JavaPlugin implements Listener {
@@ -21,45 +18,29 @@ public final class Azu2 extends JavaPlugin implements Listener {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
-
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-
-        if (cmd.getName().equalsIgnoreCase("azuheal")) {
-            if (args.length <= 0) return true;
-            if (args[0].equalsIgnoreCase("heal")) {
-                if (!sender.hasPermission("Azu2.permission.Admin")) {
-
-                    int Heal = (int) player.getHealth();
-                    if ((Heal < 20)) {
-                        player.sendMessage("HPを回復しました");
-                        player.setHealth(20 - Heal);
-                        return true;
-                    }
-                    if ((Heal > 20)) {
-                        player.sendMessage("HPを回復しました");
-                        player.setHealth(Heal - 20);
-                        return true;
-                    }
+        if (command.getLabel().equals("azuheal")) {
+            if (args.length == 1) {
+                double heal = Integer.parseInt(args[0]);
+                double amount = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+                double mheal = player.getHealth();
+                if (!(heal < 0)) {
+                    double balheal = ((amount - heal) - mheal);
+                    player.setHealth(heal + mheal + balheal);
+                    sender.sendMessage("指定した分HPを回復しました！");
                 }
             }
-
+            if (args.length == 0) {
+                if (sender.hasPermission("Azu2.permission.Admin")) {
+                    double amount = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+                    if (player.getHealth() != 0) {
+                        player.setHealth(amount);
+                        sender.sendMessage("HPを回復しました！");
+                        }
+                    }
+                }
         }
         return true;
-    }
-
-    @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, Command command, @NotNull String alias, String[] args) {
-        if (!command.getName().equalsIgnoreCase("azuheal")) return super.onTabComplete(sender, command, alias, args);
-        if (args.length == 1) {
-            if (sender.hasPermission("TheSlowHalloween.permission.Admin")) {
-                if (args[0].length() == 0) {
-                    return Collections.singletonList("heal");
-                } else if ("heal".startsWith(args[0])) {
-                    return Collections.singletonList("heal");
-                }
-            }
-        }
-        return super.onTabComplete(sender, command, alias, args);
     }
 }
